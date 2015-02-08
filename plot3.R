cca69592fbdee9c260bfc4b208f0d8c40680b156
Mark Bishop <mbishop@ksbird.net>
2015-02-08 13:45:41 -0600

    Changed read.table to only read necessary rows

--- plot3.R
+++ plot3.R
@@ -1,26 +1,24 @@
 ## R Script to produce a line plot of Energy sub metering for the days of
 ## Feb. 1, 2007 and Feb. 2, 2007
+## skipping lines and reading only needed rows speeds up read.table()
+## considerably
 
 cc    <- c("character", "character", "numeric", "numeric", "numeric",
            "numeric", "numeric", "numeric", "numeric" )
 fname <- "data/household_power_consumption.txt"
-dat   <- read.table(fname, header = TRUE, sep = ';', na.strings = '?',
-                    nrows = 2075265, colClasses = cc)
-dat2  <- with(dat, dat[Date == "1/2/2007" | Date == "2/2/2007",])
-dat2$Date <- strptime(dat2$Date, "%d/%m/%Y")
+colnames <- scan(fname, nlines = 1, sep = ";", what = "character")
+dat   <- read.table(fname, col.names = colnames, sep = ';', na.strings = '?',
+                    skip = 66637, nrows = 2880, colClasses = cc)
+dat$DateTime <- strptime(paste(dat$Date,dat$Time), "%d/%m/%Y %H:%M:%S")
 secPerDay <- 60 * 60 * 24
-dayseq <- seq(min(dat2$Date), max(dat2$Date)+secPerDay, by = "day")
-fpng   <- "./ExData_Plotting1/plot3.png"
-days   <- strftime(dayseq,"%a")
-opar   <- par(no.readonly = TRUE)
-png(file = fpng, width = 480, height = 480, units = "px")
-par(mar = c(5,4,4,1))
-plot(dat2$Sub_metering_1,xaxt = "n",type = "l",
+dayseq <- seq(min(dat$DateTime), max(dat$DateTime)+secPerDay, by = "day")
+png(file = "./ExData_Plotting1/plot3.png", width=480, height=480, units="px")
+plot(dat$DateTime, dat$Sub_metering_1,xaxt = "n",type = "l",
      ylab = "Energy sub metering",
      xlab = "")
-lines(dat2$Sub_metering_2,col="red")
-lines(dat2$Sub_metering_3,col="blue")
-axis(1, at = c(1,1440, 2880), label = days)
+lines(dat$DateTime, dat$Sub_metering_2,col="red")
+lines(dat$DateTime, dat$Sub_metering_3,col="blue")
+axis.POSIXct(1, at = dayseq, "%a")
 legend("topright", col=c("black","red","blue"),lty=c(1,1,1),
        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 dev.off()
